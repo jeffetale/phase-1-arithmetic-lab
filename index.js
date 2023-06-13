7006a3c7190c32973cdb6f73f070f301a67a72a0
jeff <jeff.etale@student.moringaschool.com>
2023-06-13 21:08:14 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.round(Math.random() * 10 ) ;
+console.log(random);
+
+let num3 = 36;
+let num4 = 8;
+let mod = num3 % num4;
+console.log(mod);
+
+let fiveNumbers = [12,6,9,13,20];
+let max = Math.max(...fiveNumbers);
+console.log(max);
+
+
+
